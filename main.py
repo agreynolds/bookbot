b33f8cc710c2826c4d69245408fd34f6340ff2b7
agreynolds <austingreynolds@gmail.com>
2024-01-17 19:42:49 -0800

    implemented character counter as count_letters + prints a lot now

--- main.py
+++ main.py
@@ -3,16 +3,34 @@ def main():
     
     book_contents = read_book(book_path)
 
-    count = word_count(book_contents)
-    print(count)
+    word_count = count_words(book_contents)
+    print(f"{book_path} has {word_count} words.")
+    print("---------")
+    letter_count = count_letters(book_contents)
+    for key in letter_count:
+        if letter_count[key] == 1:
+            print(f"{book_path} has {letter_count[key]} {key} character.")
+        else: 
+            print(f"{book_path} has {letter_count[key]} {key} characters.")
+
 
 def read_book(book_path):
     with open(book_path) as book:
         book_contents = book.read()
     return book_contents
 
-def word_count(text):
+def count_words(text):
     words = text.split()
     return len(words)
+
+def count_letters(text):
+    text = text.lower()
+    letter_dictionary = {}
+    for char in text:
+        if char in letter_dictionary:
+            letter_dictionary[char] += 1
+        else: 
+            letter_dictionary[char] = 1
+    return letter_dictionary        
 
 main()
